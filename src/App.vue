1c21c0cca66ff88136a3bccb8332797f484d6d17
marcterradas <terradas.marc@gmail.com>
2021-12-08 16:18:23 +0100

    update styles

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,16 @@
 <template>
-    <div class="">
-    <div class="">
-        <Editor></Editor>
+    <div class="h-full grid grid-cols-2 gap-0">
+        <div>
+            <Editor></Editor>
+        </div>
+        <div>
+            <Result></Result>
+        </div>
     </div>
-    <div class="">
-        <Result></Result>
-    </div>
-    </div>
+
 </template>
 
 <script setup lang="ts">
 import Editor from './components/Editor.vue'
 import Result from './components/Result.vue'
 </script>
-
-<style lang="scss">
-@import './assets/styles/reset.css';
-body {
-    height: 100%;
-}
-</style>
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import './assets/styles/reset.css'
 import './assets/styles/index.css'
 
 createApp(App).mount('#app')
