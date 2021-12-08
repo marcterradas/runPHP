266408f6747e5cc25377ac659657da9a204234ec
marcterradas <terradas.marc@gmail.com>
2021-12-08 16:48:43 +0100

    add prettier

---
+++ prettier.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    trailingComma: 'none',
+    tabWidth: 4,
+    semi: false,
+    singleQuote: true,
+    bracketSpacing: true,
+    jsxBracketSameLine: true,
+    useTabs: false,
+    printWidth: 1000
+}
