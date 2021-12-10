f46d1cc9d92a100846268c573edef5794b8d11bb
marcterradas <terradas.marc@gmail.com>
2021-12-10 17:20:34 +0100

    init editor :)

--- src/components/Editor.vue
+++ src/components/Editor.vue
@@ -1,3 +1,34 @@
-<template>Editor</template>
+<template>
+    <div id="editor" ref="container" class="h-full"></div>
+</template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { onMounted, ref, onUnmounted, watch, toRefs } from 'vue'
+import { useResizeObserver, useStorage, useDebounceFn } from '@vueuse/core'
+
+import * as monaco from 'monaco-editor'
+import editorWorker from 'monaco-editor/esm/vs/editor/editor.worker?worker'
+
+self.MonacoEnvironment = {
+    getWorker(_: string, label: string) {
+        return new editorWorker()
+    }
+}
+
+const initialEditorValue = {}
+
+const container = ref<HTMLDivElement | null>(null)
+
+let editor: monaco.editor.IStandaloneCodeEditor
+
+const editorState = useStorage<Record<string, any>>('editor-state', {})
+const editorValue = useStorage<Record<string, any>>('editor-value', initialEditorValue)
+
+const emit = defineEmits<(e: 'change', payload: typeof editorValue.value) => void>()
+
+onMounted(() => {
+    editor = monaco.editor.create(container.value!, {})
+
+    emit('change', editorValue.value)
+})
+</script>
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,19 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+
 const path = require('path')
+const prefix = 'monaco-editor/esm/vs'
 
 export default defineConfig({
     base: path.resolve(__dirname, './dist/'),
-    plugins: [vue()]
+    plugins: [vue()],
+    build: {
+        rollupOptions: {
+            output: {
+                manualChunks: {
+                    editorWorker: [`${prefix}/editor/editor.worker`]
+                }
+            }
+        }
+    }
 })
