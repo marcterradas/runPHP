51a460e25d4f8faa863231328eb1d1db51f950e0
marcterradas <terradas.marc@gmail.com>
2021-12-12 13:09:35 +0100

    remove unused code

--- src/components/Editor.vue
+++ src/components/Editor.vue
@@ -4,17 +4,14 @@
 
 <script setup lang="ts">
 import { onMounted, ref } from 'vue'
-import { useStorage, useDebounceFn } from '@vueuse/core'
+import { useDebounceFn } from '@vueuse/core'
 import { editorDefaultConfiguration, editorDefaultPosition } from '../utils/defaultConfigurations'
 import * as monaco from 'monaco-editor'
 
-const initialEditorValue = {}
 const container = ref<HTMLDivElement | null>(null)
+const emit = defineEmits<(e: 'change', payload: String) => void>()
 
 let editor: monaco.editor.IStandaloneCodeEditor
-
-const editorValue = useStorage<Record<string, any>>('editor-value', initialEditorValue)
-const emit = defineEmits<(e: 'change', payload: String) => void>()
 
 onMounted(() => {
     editor = monaco.editor.create(container.value!, editorDefaultConfiguration())
