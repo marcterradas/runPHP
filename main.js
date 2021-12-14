cbe23fde82bfa4b055fadf55da81c04a858f69b2
marcterradas <terradas.marc@gmail.com>
2021-12-14 21:29:20 +0100

    electron execute php code! :)

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 const { app, BrowserWindow } = require('electron')
 const path = require('path')
 
+const { promisify } = require('util')
+const exec = promisify(require('child_process').exec)
+
 function createWindow() {
     const win = new BrowserWindow({
         width: 1920,
@@ -12,8 +15,21 @@ function createWindow() {
 
     win.loadFile('dist/index.html')
     win.openDevTools()
+
+    test()
 }
 
 app.whenReady().then(() => {
     createWindow()
 })
+
+const test = async () => {
+    const code = 'var_dump(1111);'
+    const query = `php -r "${code}"`
+    try {
+        const { stdout, stderr } = await exec(query)
+        console.log(stdout)
+    } catch (error) {
+        console.log(error)
+    }
+}
