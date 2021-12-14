b36e9a96e08e6db35f644ffd93ca4d8f3c867cbe
marcterradas <terradas.marc@gmail.com>
2021-12-14 20:30:36 +0100

    remove unused functions

--- main.js
+++ main.js
@@ -16,16 +16,4 @@ function createWindow() {
 
 app.whenReady().then(() => {
     createWindow()
-
-    app.on('activate', () => {
-        if (BrowserWindow.getAllWindows().length === 0) {
-            createWindow()
-        }
-    })
-})
-
-app.on('window-all-closed', () => {
-    if (process.platform !== 'darwin') {
-        app.quit()
-    }
 })
