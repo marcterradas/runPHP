b282a9e43e2089ea25605247c769f19a7ba7b403
marcterradas <terradas.marc@gmail.com>
2021-12-18 16:40:52 +0100

    format document

--- preload.js
+++ preload.js
@@ -11,34 +11,36 @@ window.addEventListener('DOMContentLoaded', () => {
         replaceText(`${type}-version`, process.versions[type])
     }
 
-    const result = document.getElementById('result')
-    const error = document.getElementById('error')
     const intervalTime = 1000
 
-    setInterval(() => {
-        const code = localStorage.getItem('code')
-        const executedCode = localStorage.getItem('executedCode')
-
-        if (code == '' && code != executedCode) {
-            result.innerHTML = ''
-            error.innerHTML = ''
-            localStorage.setItem('executedCode', '')
-        }
-
-        if (code != '' && code != executedCode) {
-            const command = spawn('php', ['-r', code])
-
-            command.stdout.on('data', (data) => {
-                error.innerHTML = ''
-                result.innerHTML = data
-            })
-
-            command.stderr.on('data', (data) => {
-                result.innerHTML = ''
-                error.innerHTML = data
-            })
-
-            localStorage.setItem('executedCode', code)
-        }
-    }, intervalTime)
+    setInterval(executePHPCode, intervalTime)
 })
+
+const executePHPCode = () => {
+    const result = document.getElementById('result')
+    const error = document.getElementById('error')
+    const code = localStorage.getItem('code')
+    const executedCode = localStorage.getItem('executedCode')
+
+    if (code == '' && code != executedCode) {
+        result.innerHTML = ''
+        error.innerHTML = ''
+        localStorage.setItem('executedCode', '')
+    }
+
+    if (code != '' && code != executedCode) {
+        const command = spawn('php', ['-r', code])
+
+        command.stdout.on('data', (data) => {
+            error.innerHTML = ''
+            result.innerHTML = data
+        })
+
+        command.stderr.on('data', (data) => {
+            result.innerHTML = ''
+            error.innerHTML = data
+        })
+
+        localStorage.setItem('executedCode', code)
+    }
+}
