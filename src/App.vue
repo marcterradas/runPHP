d45c8dbe550237cc731d5e38bb3206b1621266cb
marcterradas <terradas.marc@gmail.com>
2021-12-18 13:52:00 +0100

    save code changes to localstorage

--- src/App.vue
+++ src/App.vue
@@ -14,6 +14,6 @@ import Editor from './components/Editor.vue'
 import Result from './components/Result.vue'
 
 const onChange = (code: string) => {
-    console.log(code)
+    localStorage.setItem('code', code)
 }
 </script>
