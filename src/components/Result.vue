43ffe6fd34ab0cedec721dfb7b67d8d8196489af
marcterradas <terradas.marc@gmail.com>
2021-12-18 15:38:35 +0100

    remove placeholder

--- src/components/Result.vue
+++ src/components/Result.vue
@@ -1,5 +1,5 @@
 <template>
-    <div id="result" class="text-vs-white text-base">result</div>
+    <div id="result" class="text-vs-white text-base"></div>
 </template>
 
 <script setup lang="ts"></script>
