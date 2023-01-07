617a5f3af007c48e081a8a87ee7696d07b4a79fb
Marc Terradas <terradas.marc@gmail.com>
2023-01-07 17:43:10 +0100

    print result

--- src/components/Result.vue
+++ src/components/Result.vue
@@ -1,8 +1,12 @@
 <script setup lang="ts">
+import { storeToRefs } from 'pinia'
 import ActionButton from '@/components/ActionButton.vue'
+import { useResultStore } from '@/store/result'
+
+const { result } = storeToRefs(useResultStore())
 </script>
 
 <template>
-    <div class="text-vs-white"></div>
+    <div class="text-vs-white">{{ result }}</div>
     <ActionButton />
 </template>
