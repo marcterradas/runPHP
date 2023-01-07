9a8b7a1dea1c23ac38e1f6da092e165883b245ee
Marc Terradas <terradas.marc@gmail.com>
2023-01-07 16:25:43 +0100

    update result store

--- src/store/result.ts
+++ src/store/result.ts
@@ -1,12 +1,10 @@
 import { defineStore } from 'pinia'
+import { ref } from 'vue'
 
-export const useResultStore = defineStore('result', {
-    state: () => ({
-        value: '',
-    }),
-    actions: {
-        edit(newValue: string) {
-            this.value = newValue
-        },
-    },
+export const useResultStore = defineStore('result', () => {
+    const result = ref('')
+    function edit(newValue: string) {
+        result.value = newValue
+    }
+    return { result, edit }
 })
