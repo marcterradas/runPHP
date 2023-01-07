19b0344a9e6a5416dc9b267b2d8933b1a429d591
Marc Terradas <terradas.marc@gmail.com>
2023-01-07 15:36:04 +0100

    create action button

---
+++ src/components/ActionButton.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts"></script>
+
+<template>
+    <button
+        class="absolute bottom-2 right-2 p-0 w-12 h-12 bg-blue-400 rounded-full hover:bg-blue-500 active:shadow-lg mouse shadow transition ease-in duration-200 focus:outline-none flex justify-center items-center"
+    >
+        <img src="/right-arrow.svg" class="w-8 h-8" alt="action" />
+    </button>
+</template>
