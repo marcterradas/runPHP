df5dc5aaaa6ffccbf75e7aca4bc16196cfcb7bef
Marc Terradas <terradas.marc@gmail.com>
2023-01-07 16:37:38 +0100

    update editor store

--- src/store/editor.ts
+++ src/store/editor.ts
@@ -3,8 +3,8 @@ import { ref } from 'vue'
 
 export const useResultStore = defineStore('editor', () => {
     const editor = ref('')
-    function edit(newValue: string) {
+    function editEditor(newValue: string) {
         editor.value = newValue
     }
-    return { editor, edit }
+    return { editor, editEditor }
 })
