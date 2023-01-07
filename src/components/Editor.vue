5440d4d5bb1e0fb9be1013fd47b06bf19903d6d6
Marc Terradas <terradas.marc@gmail.com>
2023-01-07 17:09:10 +0100

    save user input to store

--- src/components/Editor.vue
+++ src/components/Editor.vue
@@ -1,14 +1,20 @@
 <script setup lang="ts">
 import { onMounted, ref } from 'vue'
-import { configuration, position } from '../editor-configuration'
 import * as monaco from 'monaco-editor'
+import { configuration, position } from '@/editor-configuration'
+import { useEditorStore } from '@/store/editor'
 
 const container = ref<HTMLDivElement | null>(null)
-// const emit = defineEmits<(e: 'change', payload: string) => void>()
+const editorStore = useEditorStore()
+const { updateEditorValue } = editorStore
+
 const setUpEditor = (): void => {
     const editor = monaco.editor.create(container.value!, configuration())
     editor.focus()
     editor.setPosition(position())
+    editor.onDidChangeModelContent(() => {
+        updateEditorValue(editor.getValue())
+    })
 }
 onMounted(setUpEditor)
 </script>
