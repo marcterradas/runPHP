bd27f4d30ec462edb8a003bd68261309c45368c9
Marc Terradas <terradas.marc@gmail.com>
2023-01-07 10:29:18 +0100

    init editor

--- src/components/Editor.vue
+++ src/components/Editor.vue
@@ -1,5 +1,18 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { onMounted, ref } from 'vue'
+import { configuration, position } from '../editor-configuration'
+
+import * as monaco from 'monaco-editor'
+const container = ref<HTMLDivElement | null>(null)
+// const emit = defineEmits<(e: 'change', payload: string) => void>()
+const setUpEditor = (): void => {
+    const editor = monaco.editor.create(container.value!, configuration())
+    editor.focus()
+    editor.setPosition(position())
+}
+onMounted(setUpEditor)
+</script>
 
 <template>
-    <div class="text-vs-white">Editor ...</div>
+    <div id="editor" ref="container" class="h-full"></div>
 </template>
