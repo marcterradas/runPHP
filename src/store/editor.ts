b23bd4775ae11ed092592acc57319813f589e760
Marc Terradas <terradas.marc@gmail.com>
2023-01-07 16:54:19 +0100

    change store function name

--- src/store/editor.ts
+++ src/store/editor.ts
@@ -1,7 +1,7 @@
 import { defineStore } from 'pinia'
 import { ref } from 'vue'
 
-export const useResultStore = defineStore('editor', () => {
+export const useEditorStore = defineStore('editor', () => {
     const editor = ref('')
     function editEditor(newValue: string) {
         editor.value = newValue
