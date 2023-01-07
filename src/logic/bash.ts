c7ce1655201e29e1f682c0f56cccce6ffb19e573
Marc Terradas <terradas.marc@gmail.com>
2023-01-07 17:42:43 +0100

    test reactivity

--- src/logic/bash.ts
+++ src/logic/bash.ts
@@ -1,5 +1,5 @@
 export async function executePHPCode(input: string): Promise<string> {
-    let result: string = 'example ...'
+    let result: string = Math.random().toString()
 
     return result
 }
