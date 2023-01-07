d9aef87028fdfb697732fdc038067d416c659466
Marc Terradas <terradas.marc@gmail.com>
2023-01-07 10:12:26 +0100

    add custom colors to tailwind

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,11 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './index.html',
-    './src/**/*.{js,ts,jsx,tsx,vue}'
-  ],
-  theme: {
-    extend: {}
-  },
-  plugins: []
+    content: ['./index.html', './src/**/*.{js,ts,jsx,tsx,vue}'],
+    theme: {
+        extend: {
+            colors: {
+                'vs-dark': '#1e1e1e',
+                'vs-white': '#d4d4d4',
+            },
+        },
+    },
+    plugins: [],
 }
