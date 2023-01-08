6c7771cd2a2dc5080c5adb47b01d2a180f8f5b8f
Marc Terradas <terradas.marc@gmail.com>
2023-01-08 09:34:35 +0100

    execute php commands

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -8,23 +8,13 @@ fn execute_php(input: &str) -> String {
     use std::process::{Output, Command};
     use std::str;
     
-    let output: Output = if cfg!(target_os = "windows") {
-        Command::new("cmd")
-                .args(["/C", "echo hello"])
-                .output()
-                .expect("failed to execute process")
-    } else {
-        Command::new("sh")
-                .arg("-c")
-                .arg("echo hello")
-                .output()
-                .expect("failed to execute process")
-    };
-    
+    let output: Output = Command::new("php")
+    .args(["-r", input])
+    .output()
+    .expect("failed to execute process");
+
     let hello: Vec<u8> = output.stdout;
     let s: String = String::from(str::from_utf8(&hello).unwrap());
-
-    println!("{}",input);
 
     return s;
     
