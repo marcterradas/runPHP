f4c27cb4afc15247432133876e904bcdb93a5b9b
Marc Terradas <terradas.marc@gmail.com>
2023-01-08 09:28:38 +0100

    init logic execute terminal command

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,15 +3,36 @@
     windows_subsystem = "windows"
 )]
 
-// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
+fn execute_php(input: &str) -> String {
+    use std::process::{Output, Command};
+    use std::str;
+    
+    let output: Output = if cfg!(target_os = "windows") {
+        Command::new("cmd")
+                .args(["/C", "echo hello"])
+                .output()
+                .expect("failed to execute process")
+    } else {
+        Command::new("sh")
+                .arg("-c")
+                .arg("echo hello")
+                .output()
+                .expect("failed to execute process")
+    };
+    
+    let hello: Vec<u8> = output.stdout;
+    let s: String = String::from(str::from_utf8(&hello).unwrap());
+
+    println!("{}",input);
+
+    return s;
+    
 }
 
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![execute_php])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
