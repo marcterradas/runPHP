dd1e8b0f8bba807b8d91b432a00af087d7b17973
Marc Terradas <terradas.marc@gmail.com>
2023-01-08 09:28:23 +0100

    execute rust funcion execute php

--- src/logic/bash.ts
+++ src/logic/bash.ts
@@ -1,6 +1,6 @@
 import { invoke } from '@tauri-apps/api/tauri'
 
 export async function executePHPCode(input: string): Promise<string> {
-    let result: string = await invoke('greet', { name: input })
+    let result: string = await invoke('execute_php', { input })
     return result
 }
